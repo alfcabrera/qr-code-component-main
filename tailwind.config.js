11b1d0049e2da3d11fdc3bb58d412e344e75ad5d
Mario Cabrera <mac25us@gmail.com>
2024-01-11 09:58:05 -0400

    ✨ project finished

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        fontFamily: {
+            sans: ["'Outfit'", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
